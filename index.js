6a47386a6976a111e6e048b449d0c120800d8724
Andre Vermeulen <andre.vermeulen@gmail.com>
2014-11-17 07:14:50 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var express = require('express'),
+    exphbs  = require('express-handlebars'),
+    mysql = require('mysql'), 
+    myConnection = require('express-myconnection'),
+    bodyParser = require('body-parser'),
+    products = require('./routes/products');
+
+var app = express();
+
+var dbOptions = {
+      host: 'localhost',
+      user: 'constructer',
+      password: 'password',
+      port: 3306,
+      database: 'construct_me'
+};
+
+//setup template handlebars as the template engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+//setup middleware
+app.use(myConnection(mysql, dbOptions, 'single'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+//setup the handlers
+app.get('/products', products.show);
+app.get('/products/edit/:id', products.get);
+app.post('/products/update/:id', products.update);
+app.post('/products/add', products.add);
+//this should be a post but this is only an illustration of CRUD - not on good practices
+app.get('/products/delete/:id', products.delete);
+
+//start everything up
+app.listen(3000, function () {
+    console.log('express-handlebars example server listening on: 3000');
+});
---
+++ routes/products.js
@@ -0,0 +1,84 @@
+
+/***
+ * A very basic CRUD example using MySQL
+ */	
+
+//todo - fix the error handling
+
+exports.show = function (req, res, next) {
+	req.getConnection(function(err, connection){
+		if (err) 
+			return next(err);
+		connection.query('SELECT * from products', [], function(err, results) {
+        	if (err) return next(err);
+
+    		res.render( 'home', {
+    			products : results
+    		});
+      });
+	});
+};
+
+exports.add = function (req, res, next) {
+	req.getConnection(function(err, connection){
+
+		var input = JSON.parse(JSON.stringify(req.body));
+		var data = {
+            description : input.description,
+        };
+
+		if (err) 
+			return next(err);
+		
+		connection.query('insert into products set ?', data, function(err, results) {
+        	if (err)
+              console.log("Error inserting : %s ",err );
+         
+          	res.redirect('/products');
+      	});
+	});
+};
+
+exports.get = function(req, res, next){
+	var id = req.params.id;
+	req.getConnection(function(err, connection){
+
+		connection.query('SELECT * FROM products WHERE id = ?', [id], function(err,rows){
+			if(err){
+    			console.log("Error Selecting : %s ",err );
+			}
+
+			res.render('edit',{page_title:"Edit Customers - Node.js", data : rows[0]});      
+		}); 
+	});
+};
+
+exports.update = function(req, res, next){
+
+	var data = JSON.parse(JSON.stringify(req.body));
+    var id = req.params.id;
+    req.getConnection(function(err, connection){
+    	connection.query('UPDATE products SET ? WHERE id = ?', [data, id], function(err, rows){
+    		
+    		if (err){
+              console.log("Error Updating : %s ",err );
+    		}
+          	res.redirect('/products');
+    	});
+    });
+};
+
+exports.delete = function(req, res, next){
+	var id = req.params.id;
+	req.getConnection(function(err, connection){
+
+		connection.query('DELETE FROM products WHERE id = ?', [id], function(err,rows){
+			if(err){
+    			console.log("Error Selecting : %s ",err );
+			}
+			res.redirect('/products');
+		});
+		 
+	});
+};
+
