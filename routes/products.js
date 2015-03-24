5afe7e79f72e1a057ed3b474094075d51a63762f
Andre Vermeulen <andre.vermeulen@gmail.com>
2015-03-24 16:15:37 +0200

    Update products.js
    changed code formatting

--- routes/products.js
+++ routes/products.js
@@ -21,33 +21,30 @@ exports.show = function (req, res, next) {
 
 exports.add = function (req, res, next) {
 	req.getConnection(function(err, connection){
-
+		if (err){ 
+			return next(err);
+		}
+		
 		var input = JSON.parse(JSON.stringify(req.body));
 		var data = {
-            description : input.description,
-        };
-
-		if (err) 
-			return next(err);
-		
+            		description : input.description,
+        	};
 		connection.query('insert into products set ?', data, function(err, results) {
-        	if (err)
-              console.log("Error inserting : %s ",err );
+        		if (err)
+              			console.log("Error inserting : %s ",err );
          
-          	res.redirect('/products');
-      	});
+          		res.redirect('/products');
+      		});
 	});
 };
 
 exports.get = function(req, res, next){
 	var id = req.params.id;
 	req.getConnection(function(err, connection){
-
 		connection.query('SELECT * FROM products WHERE id = ?', [id], function(err,rows){
 			if(err){
-    			console.log("Error Selecting : %s ",err );
+    				console.log("Error Selecting : %s ",err );
 			}
-
 			res.render('edit',{page_title:"Edit Customers - Node.js", data : rows[0]});      
 		}); 
 	});
@@ -56,29 +53,27 @@ exports.get = function(req, res, next){
 exports.update = function(req, res, next){
 
 	var data = JSON.parse(JSON.stringify(req.body));
-    var id = req.params.id;
-    req.getConnection(function(err, connection){
-    	connection.query('UPDATE products SET ? WHERE id = ?', [data, id], function(err, rows){
+    	var id = req.params.id;
+    	req.getConnection(function(err, connection){
+    		connection.query('UPDATE products SET ? WHERE id = ?', [data, id], function(err, rows){
+    			if (err){
+              			console.log("Error Updating : %s ",err );
+    			}
+          		res.redirect('/products');
+    		});
     		
-    		if (err){
-              console.log("Error Updating : %s ",err );
-    		}
-          	res.redirect('/products');
-    	});
     });
 };
 
 exports.delete = function(req, res, next){
 	var id = req.params.id;
 	req.getConnection(function(err, connection){
-
 		connection.query('DELETE FROM products WHERE id = ?', [id], function(err,rows){
 			if(err){
-    			console.log("Error Selecting : %s ",err );
+    				console.log("Error Selecting : %s ",err );
 			}
 			res.redirect('/products');
 		});
-		 
 	});
 };
 
