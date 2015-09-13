d2aad65e923fbbab425905932825dc7aa62315a9
avermeulen <andre.vermeulen@gmail.com>
2015-09-13 08:46:11 +0200

    improved error handling

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
 //setup the handlers
 app.get('/', products.show);
 app.get('/products', products.show);
@@ -39,6 +44,8 @@ app.get('/products/add', products.showAdd);
 app.post('/products/add', products.add);
 //this should be a post but this is only an illustration of CRUD - not on good practices
 app.get('/products/delete/:id', products.delete);
+
+app.use(errorHandler);
 
 //configure the port number using and environment number
 var portNumber = process.env.CRUD_PORT_NR || 3000;
