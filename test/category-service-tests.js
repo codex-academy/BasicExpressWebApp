bb98c58c9238a21ab39926cd0814d822712e6f66
avermeulen <andre.vermeulen@gmail.com>
2018-08-20 17:54:50 +0200

    added a category update test

--- test/category-service-tests.js
+++ test/category-service-tests.js
@@ -3,7 +3,7 @@ const CategoryService = require('../services/category-service');
 const pg = require("pg");
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/my_products';
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/my_products_tests';
 
 const pool = new Pool({
     connectionString
@@ -16,17 +16,29 @@ describe('The basic database web app', function(){
         await pool.query("delete from categories;");
     });
 
-    it('should pass the db test', async function(){
-
+    it('should able to add a category', async function(){
         let categoryService = CategoryService(pool);
         await categoryService.add({
             description : "Diary"
         });
-
         let categories = await categoryService.all();
-
         assert.equal(1, categories.length);
+    });
 
+    it('should able to update a category', async function(){
+        // assemble
+        let categoryService = CategoryService(pool);
+        let category = await categoryService.add({
+            description : "Diary"
+        });
+
+        // act
+        category.description = 'Milk products';
+        await categoryService.update(category);
+        
+        // assert
+        let updateCategory = await categoryService.get(category.id);
+        assert.equal('Milk products', updateCategory.description);
     });
 
     after(function(){
